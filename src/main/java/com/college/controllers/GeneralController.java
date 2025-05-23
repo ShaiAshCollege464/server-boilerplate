11087e200cbe7424885767f3f9579c8b413bc61a
Shai <Shai@gmail.com>
2025-05-23 08:44:58 +0300

    Initial commit

---
+++ src/main/java/com/college/controllers/GeneralController.java
@@ -0,0 +1,18 @@
+package com.college.controllers;
+
+import com.college.entities.Task;
+import com.college.responses.BasicResponse;
+import com.college.responses.TasksReponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+@RestController
+public class GeneralController {
+
+
+}
---
+++ src/main/java/com/college/utils/ApiUtils.java
@@ -0,0 +1,5 @@
+package com.college.utils;
+
+public class ApiUtils {
+
+}
