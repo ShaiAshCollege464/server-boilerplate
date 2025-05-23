bb3669be744876293d467b4d6176effd6f3e2ddf
Shai <Shai@gmail.com>
2025-05-23 08:52:46 +0300

    general

--- src/main/java/com/college/controllers/GeneralController.java
+++ src/main/java/com/college/controllers/GeneralController.java
@@ -1,14 +1,8 @@
 package com.college.controllers;
 
-import com.college.entities.Task;
-import com.college.responses.BasicResponse;
-import com.college.responses.TasksReponse;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
 
 
 @RestController
