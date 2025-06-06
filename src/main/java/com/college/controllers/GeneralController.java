4207c2f1cbc32448bc080d6ee3c6b45f57cbb458
shaig <shai@upvote>
2025-06-06 11:16:40 +0300

    db integration

--- src/main/java/com/college/controllers/GeneralController.java
+++ src/main/java/com/college/controllers/GeneralController.java
@@ -5,6 +5,8 @@ import com.college.entities.User;
 import com.college.responses.AvailableResponse;
 import com.college.responses.BasicResponse;
 import com.college.responses.UsersResponse;
+import com.college.utils.DbUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,20 +18,17 @@ public class GeneralController {
 
     private List<User> allUsers = new ArrayList<>();
 
+    @Autowired
+    private DbUtils dbUtils;
+
     @RequestMapping ("/users")
     public List<User> getAllUsers () {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return allUsers;
+        return dbUtils.fetchAllUsers();
     }
 
     @RequestMapping("/add-user")
     public void addUser (String username, String password) {
-        User user = new User(username, password);
-        allUsers.add(user);
+        dbUtils.addUser(username, password);
     }
 
 }
