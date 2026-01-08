faa531bd92c3b9645202f6dfe93104059efda94a
Shai <shai>
2026-01-08 15:54:55 +0200

    remove unused

--- src/main/java/com/college/controllers/GeneralController.java
+++ src/main/java/com/college/controllers/GeneralController.java
@@ -1,19 +1,11 @@
 package com.college.controllers;
 
 
-import com.college.entities.User;
-import com.college.responses.BasicResponse;
-import com.college.responses.UserResponse;
-import com.college.responses.UsersResponse;
 import com.college.utils.DbUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.college.utils.Errors.*;
 
 
 @RestController
@@ -24,82 +16,6 @@ public class GeneralController {
 
     @PostConstruct
     public void init() {
-    }
-
-
-    @RequestMapping("/delete")
-    public BasicResponse delete(int id) {
-        if (id != 0) {
-            dbUtils.delete(id);
-            return new BasicResponse(true, null);
-
-        } else {
-            return new BasicResponse(false, ERROR_MISSING_USERNAME);
-        }
-    }
-
-
-    @RequestMapping("/login")
-    public BasicResponse login(String username, String password) {
-        if (username != null) {
-            if (password != null) {
-                if (usernameExistsOnTheDn(username)) {
-                    User user = getUser(username, password);
-                    if (user != null) {
-                        return new UserResponse(true, null, user);
-                    } else {
-                        return new BasicResponse(false, ERROR_WRONG_PASSWORD);
-                    }
-                } else {
-                    return new BasicResponse(false, ERROR_USERNAME_NOT_SIGNED_UP);
-                }
-            } else {
-                return new BasicResponse(false, ERROR_MISSING_PASSWORD);
-            }
-        } else {
-            return new BasicResponse(false, ERROR_MISSING_USERNAME);
-        }
-    }
-
-    @RequestMapping("returnAllusers")
-    public BasicResponse returnAllusers (){
-        return new UsersResponse(true, null, dbUtils.getAllUsers());
-    }
-
-    @RequestMapping("sign-up")
-    public BasicResponse createUser(String username, String password) {
-        if (username != null) {
-            if (password != null) {
-                if (!usernameExistsOnTheDn(username)) {
-                    User newUser = new User(username, password);
-                    dbUtils.insertUser(newUser);
-                    return new BasicResponse(true, null);
-                } else {
-                    return new BasicResponse(false, ERROR_USERNAME_TAKEN);
-                }
-            } else {
-                return new BasicResponse(false, ERROR_MISSING_PASSWORD);
-            }
-        } else {
-            return new BasicResponse(false, ERROR_MISSING_USERNAME);
-        }
-    }
-
-    private boolean usernameExistsOnTheDn(String username) {
-        return dbUtils.doesUsernameExist(username);
-    }
-
-    //CRUD
-    //Create - INSERT
-    //Read -> SELECT
-    //Update -> UPDATE
-    //Delete -> DELETE
-
-
-
-    public User getUser(String username, String password) {
-        return dbUtils.getUser(username,password);
-
     }
 
 
--- src/main/java/com/college/utils/DbUtils.java
+++ src/main/java/com/college/utils/DbUtils.java
@@ -1,6 +1,5 @@
 package com.college.utils;
 
-import com.college.entities.User;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -33,86 +32,6 @@ public class DbUtils {
         }
     }
 
-    public void insertUser(User user) {
-        try {
-            PreparedStatement preparedStatement = this.connection.prepareStatement(
-                    "INSERT INTO users (username, password) " +
-                            "VALUE (?, ?)");
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement =
-                    this.connection.prepareStatement("SELECT id, username, password FROM users");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String username = resultSet.getString("username");
-                String password = resultSet.getString("password");
-                User user = new User(username, password);
-                user.setId(id);
-                users.add(user);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return users;
-
-    }
-
-    public boolean doesUsernameExist(String username) {
-        try {
-            PreparedStatement preparedStatement = this.connection.prepareStatement(
-                    "SELECT id FROM users WHERE username = ?"
-            );
-            preparedStatement.setString(1, username);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public User getUser(String username, String password) {
-        try {
-            PreparedStatement preparedStatement = this.connection.prepareStatement("" +
-                    "SELECT id, username, password FROM users WHERE" +
-                    " username = ? AND password=?;"
-            );
-            preparedStatement.setString(1,username);
-            preparedStatement.setString(2,password);
-            ResultSet resultSet=preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return new User(username,password);
-            } else {
-                return null;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void delete (int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    this.connection.prepareStatement("DELETE FROM users WHERE id = ?");
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
 }
